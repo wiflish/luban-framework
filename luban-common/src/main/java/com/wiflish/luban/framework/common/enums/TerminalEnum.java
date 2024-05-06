b73c95f70192abf75a83426a2d17ed2e8c0361a3
wiflish <wiflish.xie@gmail.com>
2024-05-06 20:43:40 +0800

    H5网页修改为H5移动端

--- luban-common/src/main/java/com/wiflish/luban/framework/common/enums/TerminalEnum.java
+++ luban-common/src/main/java/com/wiflish/luban/framework/common/enums/TerminalEnum.java
@@ -18,7 +18,7 @@ public enum TerminalEnum implements IntArrayValuable {
     UNKNOWN(0, "未知"), // 目的：在无法解析到 terminal 时，使用它
     WECHAT_MINI_PROGRAM(10, "微信小程序"),
     WECHAT_WAP(11, "微信公众号"),
-    H5(20, "H5 网页"),
+    H5(20, "H5 移动端"),
     APP(31, "手机 App"),
     ;
 
