7842ba248eb859a8f1e93f5fa31145ecaba4639d
wiflish <wiflish.xie@gmail.com>
2024-05-30 21:58:22 +0800

    增加印尼的手机号验证规则。

--- luban-common/src/main/java/com/wiflish/luban/framework/common/util/validation/ValidationUtils.java
+++ luban-common/src/main/java/com/wiflish/luban/framework/common/util/validation/ValidationUtils.java
@@ -2,12 +2,16 @@ package com.wiflish.luban.framework.common.util.validation;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.Assert;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -18,15 +22,23 @@ import java.util.regex.Pattern;
  */
 public class ValidationUtils {
 
+    private static final Map<String, Pattern> MOBILE_MAP = MapUtil.newHashMap();
     private static final Pattern PATTERN_MOBILE = Pattern.compile("^(?:(?:\\+|00)86)?1(?:(?:3[\\d])|(?:4[0,1,4-9])|(?:5[0-3,5-9])|(?:6[2,5-7])|(?:7[0-8])|(?:8[\\d])|(?:9[0-3,5-9]))\\d{8}$");
+    private static final Pattern PATTERN_MOBILE_IDN = Pattern.compile("^\\d{6,15}$");
 
     private static final Pattern PATTERN_URL = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
 
     private static final Pattern PATTERN_XML_NCNAME = Pattern.compile("[a-zA-Z_][\\-_.0-9_a-zA-Z$]*");
 
+    static {
+        MOBILE_MAP.put("ID", PATTERN_MOBILE_IDN);
+        MOBILE_MAP.put("CN", PATTERN_MOBILE);
+    }
+
     public static boolean isMobile(String mobile) {
-        return StringUtils.hasText(mobile)
-                && PATTERN_MOBILE.matcher(mobile).matches();
+        String statCode = SpringUtil.getProperty("mall.info.state-code");
+        Pattern pattern = MOBILE_MAP.get(statCode);
+        return StringUtils.hasText(mobile) && Objects.requireNonNullElse(pattern, PATTERN_MOBILE_IDN).matcher(mobile).matches();
     }
 
     public static boolean isURL(String url) {
