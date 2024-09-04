8c87c8313525ae3439d9ddf24eb373c32ae59c86
wiflish <wiflish.xie@gmail.com>
2024-09-04 15:57:47 +0800

    payClient构造器处理

--- luban-starter-pay/luban-starter-pay-xendit/src/main/java/com/wiflish/luban/framework/pay/core/client/xendit/ewallet/XenditEWalletAstrapayPayClient.java
+++ luban-starter-pay/luban-starter-pay-xendit/src/main/java/com/wiflish/luban/framework/pay/core/client/xendit/ewallet/XenditEWalletAstrapayPayClient.java
@@ -5,8 +5,6 @@ import com.wiflish.luban.framework.pay.core.client.xendit.XenditPayClientConfig;
 import com.wiflish.luban.framework.pay.xendit.dto.payment.ChannelPropertiesDTO;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.wiflish.luban.framework.pay.core.enums.channel.PayChannelEnum.XENDIT_E_WALLET_ASTRAPAY;
-
 /**
  * Xendit Astrapay钱包支付
  *
@@ -15,8 +13,8 @@ import static com.wiflish.luban.framework.pay.core.enums.channel.PayChannelEnum.
  */
 @Slf4j
 public class XenditEWalletAstrapayPayClient extends XenditEWalletAbstractPayClient {
-    public XenditEWalletAstrapayPayClient(Long channelId, XenditPayClientConfig config) {
-        super(channelId, XENDIT_E_WALLET_ASTRAPAY.getCode(), config);
+    public XenditEWalletAstrapayPayClient(Long channelId, String channelCode, XenditPayClientConfig config) {
+        super(channelId, channelCode, config);
     }
 
     @Override
